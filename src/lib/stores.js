6a205f699cbdda86f0d9cc2dcc83b80006303334
Doug A <douganderson444@gmail.com>
2022-02-10 09:56:25 -0400

    add contacts svelte store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -4,4 +4,4 @@ export const rootCID = writable(null);
 export const ipfsNode = writable(null);
 export const CID = writable(null);
 
-export const setAccess = writable(null);
+export const contacts = writable([]);
