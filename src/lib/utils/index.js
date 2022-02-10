7f298ebd663b123c73d03d961f5c6c46112afd24
Doug A <douganderson444@gmail.com>
2022-02-10 09:55:28 -0400

    move validate fn to utils

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,4 +1,8 @@
+import b64 from 'base64-js';
+import bs58 from 'bs58';
+
 const ROOT_CID = '__ROOT_CID';
+const PUBLIC_KEY_BYTES = 32;
 
 // Pre-Init
 const LUT_HEX_4b = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
@@ -13,4 +17,42 @@ export function bufftoHex(buffer) {
 		out += LUT_HEX_8b[buffer[idx]];
 	}
 	return out;
+}
+
+export function validatePubKey(pubKey) {
+	if (!pubKey) return;
+
+	// base64 / base64URL
+	let pubKeyBytes = b64.toByteArray(pubKey);
+
+	if (pubKeyBytes.length === PUBLIC_KEY_BYTES) {
+		return pubKeyBytes;
+	}
+
+	// base58 / base58BTC
+	try {
+		let b58Bytes = bs58.decode(pubKey);
+		if (b58Bytes.length === PUBLIC_KEY_BYTES) {
+			return b58Bytes;
+		}
+	} catch (error) {}
+
+	// hex
+	const fromHexString = (hexString) =>
+		new Uint8Array(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+	let hexb58Bytes = fromHexString(pubKey);
+
+	if (hexb58Bytes.length === PUBLIC_KEY_BYTES) {
+		return hexb58Bytes;
+	}
+
+	return false;
+}
+
+export async function hexDigestMessage(message) {
+	const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+	const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
+	const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+	const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+	return hashHex;
 }
