4015a3a21f19c197fcee2756cbbd5eeb1da83d92
Doug A <douganderson444@gmail.com>
2022-02-18 09:40:50 -0400

    fix: tag name

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,7 +1,7 @@
 import ShowContacts from '$lib/templates/Contacts/ShowContacts.svelte';
 import Profile from '$lib/templates/Basic/Profile.svelte';
 
-const CONTACTS = 'Show Contacts';
+const CONTACTS = 'Contacts';
 const PROFILE = 'Profile';
 
 export const Components = {
